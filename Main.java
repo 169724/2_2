78e73e441b70031fbc6d6ac6a98c390a9712c75a
DejViczu12 <126686272+169724@users.noreply.github.com>
2023-03-29 08:24:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main{
+ public static void main(String[] args) {
+  int i,silnia=1;
+  int liczba = 12;
+  for(i=1;i<=liczba;i++){
+      silnia=silnia*i;
+  }
+  System.out.println("Silnia liczby "+liczba+" to: "+silnia);
+ }
+}
